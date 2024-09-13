7bccccfe51b43318a2637c03e2762325b0e23c50
shabane <m.mohamadshabane@gmail.com>
2024-09-14 03:12:04 +0330

    chore: add cookieParser middleware and some comments

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const texts = require('./routes/texts');
+const cookieParser = require('cookie-parser')
 
 app = express();
 
+// Installed middleWare
 app.use(express.json());
+app.use(cookieParser());
+
+// Routes
 app.use('/api/text/', texts.router);
 
 app.listen(process.env.PORT || 3000);
