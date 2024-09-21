f43ed26adf74abb84ce6f9a90ca55c99a896c00b
shabane <m.mohamadshabane@gmail.com>
2024-09-21 18:34:46 +0330

    chore: create a module to have http codes and its messages.

---
+++ utils/response_text.js
@@ -0,0 +1,17 @@
+const httpStatusCodes = {
+    301: 'Moved Permanently',
+    400: 'Bad Request',
+    401: 'Unauthorized',
+    403: 'Forbidden',
+    404: 'Not Found',
+    405: 'Method Not Allowed',
+    406: 'Not Acceptable',
+    408: 'Request Timeout',
+    411: 'Length Required',
+    431: 'Request Header Fields Too Large',
+    500: 'Internal Server Error',
+    501: 'Not Implemented',
+    507: 'Insufficient Storage',
+};
+
+module.exports = httpStatusCodes;
