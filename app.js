bf1bbc73d58a9af59e3fc29d115aa943e4660517
shabane <m.mohamadshabane@gmail.com>
2024-09-24 02:09:03 +0330

    feat: create middleware to chekc if cookies are existed

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const texts = require('./routes/texts');
 const cookieParser = require('cookie-parser')
+const links = require('./routes/links');
+const response_text = require('./utils/response_text');
 
 app = express();
 
@@ -8,7 +10,17 @@ app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+// Check if cookies are exist
+app.use((req, res, next) => {
+    if (!req.cookies.username && !req.cookies.password) {
+        res.status(401).send(response_text["401"]);
+        return;
+    }
+    next();
+});
+
 // Routes
 app.use('/api/text/', texts.router);
+app.use('/api/link/', links.router);
 
 app.listen(process.env.PORT || 3000);
