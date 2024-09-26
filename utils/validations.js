100668b32b94c38a51176ae87acccc553506dc9c
shabane <m.mohamadshabane@gmail.com>
2024-09-26 05:28:18 +0330

    chore: add explain in regx in validation

--- utils/validations.js
+++ utils/validations.js
@@ -2,6 +2,6 @@ const yup = require("yup");
 const setting = require("../settings");
 
 exports.userSchema = yup.object({
-    username: yup.string().required().max(setting.max_username_char).min(setting.min_username_char).matches(/^\S*$/),
+    username: yup.string().required().max(setting.max_username_char).min(setting.min_username_char).matches(/^\S*$/, "no white spaces in username"),
     password: yup.string().required().max(setting.max_password_char).min(setting.min_password_char),
 });
