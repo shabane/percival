c547c48331074013063bc8f9a58b963e84b62226
shabane <m.mohamadshabane@gmail.com>
2024-09-26 05:26:20 +0330

    feat: use validations for username and password validations

--- routes/users.js
+++ routes/users.js
@@ -3,32 +3,38 @@ const router = express.Router();
 respose_text = require("../utils/response_text");
 const { User } = require("../models");
 const {debug} = require("../settings");
+const { userSchema } = require("../utils/validations");
 
 router.post("/", (req, res) => {
-    User.findOne({
-        where: {
-            username: req.body.username,
-        }
-    }).then(user => {
-        if (user) {
-            res.status(406).send("User Exist!");
-            return;
-        }
+    userSchema.validate(req.body).then(() => {
+        User.findOne({
+            where: {
+                username: req.body.username,
+            }
+        }).then(user => {
+            if (user) {
+                res.status(406).send("User Exist!");
+                return;
+            }
 
-        User.create({
-            username: req.body.username,
-            password: req.body.password,
-        }).then(new_user => {
-            res.send(new_user);
-            return;
-        }).catch(err => {
-            debug(err);
-            res.status(500).send(respose_text["500"]);
-            return;
-        });
+            User.create({
+                username: req.body.username,
+                password: req.body.password,
+            }).then(new_user => {
+                res.send(new_user);
+                return;
+            }).catch(err => {
+                debug(err);
+                res.status(500).send(respose_text["500"]);
+                return;
+            });
     }).catch(err => {
         debug(err);
         res.status(500).send(respose_text["500"]);
+        return;
+    });
+    }).catch(err => {
+        res.status(400).send(err.message);
         return;
     });
 });
