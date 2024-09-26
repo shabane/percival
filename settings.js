6f9d3547fecbd43a99c5368263f12902fd6af261
shabane <m.mohamadshabane@gmail.com>
2024-09-27 01:09:35 +0330

    chore: add some variables to control file uploads

--- settings.js
+++ settings.js
@@ -12,13 +12,15 @@ exports.min_username_char = 3;
 exports.max_password_char = 256;
 exports.min_password_char = 8;
 
-exports.max_file_path_char = 128;
-
+exports.max_file_name_char = 128;
 exports.file_dest = "./user_files";
+MB = 1024 * 1024;   // This mean MB, add another `*1024` to make it GB and so on.
+exports.max_file_size = 10 * MB;  // Set Limit to 10MB.
+exports.max_file_limit = 5; // Max number of upload files limit.
 
-exports.max_link_char = 128;
+exports.max_link_char = 128; //TODO: create validation for this
 
-exports.max_user_link_char = 128;
+exports.max_user_link_char = 128; //TODO: create validation for this
 
 exports.file_expire_days = 1;
 
