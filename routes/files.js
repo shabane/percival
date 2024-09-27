5fff206e67dcff2d861515712c8ef9860cffab8b
shabane <m.mohamadshabane@gmail.com>
2024-09-27 05:14:54 +0330

    feat: save each file to a sub dir, mkdir if !dest, create the post route,

--- routes/files.js
+++ routes/files.js
@@ -1,15 +1,47 @@
 const multer = require("multer");
 const settings = require("../settings");
 const express = require("express");
-
+const { randInt } = require("../utils/base");
 const router = express.Router();
+const fs = require("fs");
+const {debug} = require("../settings");
 
 //  file handler instance.
-const file_upload_handler = multer({
-  dest: settings.file_dest,
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    let sub = Date.now().toString() + randInt(settings.max_randint_num).toString();
+    let dest = `${settings.file_dest}/${sub}/`;
+
+    if (!fs.existsSync(settings.file_dest)) {
+      fs.mkdirSync(settings.file_dest);
+    }
+
+    if (!fs.existsSync(dest)) {
+      fs.mkdirSync(dest);
+    }
+    cb(null, dest);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+let file_upload_handler = multer({
+  storage: storage,
   limits: {
     fileSize: settings.max_file_size,
     files: settings.max_file_limit,
     fieldNameSize: settings.max_file_name_char,
   },
 });
+
+
+router.post("/", file_upload_handler.array("files"  ), (req, res, next) => {
+  let files = [];
+  for (let file of req.files) {
+    files.push(file.destination);
+  }
+  res.send(files);
+});
+
+exports.router = router;
