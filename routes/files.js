19bb4dac8119164d0fd4a4ece045192c38c1ea9a
shabane <m.mohamadshabane@gmail.com>
2024-09-27 06:36:11 +0330

    feat: update database on uploading new file

--- routes/files.js
+++ routes/files.js
@@ -5,6 +5,10 @@ const { randInt } = require("../utils/base");
 const router = express.Router();
 const fs = require("fs");
 const {debug} = require("../settings");
+const { File, User_File } = require("../models");
+const find_user = require("../utils/find_user");
+const response_text = require("../utils/response_text");
+
 
 //  file handler instance.
 const storage = multer.diskStorage({
@@ -37,11 +41,52 @@ let file_upload_handler = multer({
 
 
 router.post("/", file_upload_handler.array("files"  ), (req, res, next) => {
-  let files = [];
-  for (let file of req.files) {
-    files.push(file.destination);
-  }
-  res.send(files);
+  find_user(req.cookies.username, req.cookies.password).then(user => {
+    if (!user) {
+      res.status(403).send(response_text["403"]);
+      return;
+    }
+
+    let files = [];
+    let filePromises = [];
+
+    for (let file of req.files) {
+      let filePromise = File.create({
+        path: `${file.destination}/${file.originalname}`,
+      }).then(new_file => {
+        return User_File.create({
+          user: user.id,
+          data: new_file.id,
+        }).then(user_file => {
+          files.push({
+            user: user.username,
+            file: file,
+            user_file: user_file,
+          });
+        }).catch(err => {
+          debug(err);
+          res.status(500).send(response_text["500"]);
+          return;
+        });
+      }).catch(err => {
+        debug(err);
+        res.status(500).send(response_text["500"]);
+        return;
+      });
+      filePromises.push(filePromise);
+    }
+
+    Promise.all(filePromises).then(() => {
+      res.send(files);
+    }).catch(err => {
+      debug(err);
+      res.status(500).send(response_text["500"]);
+    })
+  }).catch(err => {
+    debug(err);
+    res.status(500).send(response_text["500"]);
+    return;
+  });
 });
 
 exports.router = router;
