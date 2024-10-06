d6bbbf325ac3efcb2879f0c5d3b115a2c0a0e9a1
shabane <m.mohamadshabane@gmail.com>
2024-10-06 13:54:59 +0330

    chore: enable chores and remove checkCookies

--- app.js
+++ app.js
@@ -3,22 +3,23 @@ const texts = require('./routes/texts');
 const cookieParser = require('cookie-parser')
 const links = require('./routes/links');
 const users = require("./routes/users");
-const check_cookies = require("./utils/check_cookies");
 const files = require("./routes/files");
 const cors = require("cors");
-
 
 app = express();
 
 // Installed middleWare
-app.use(cors({origin: ["*"]}))
+app.use(cors({
+    credentials: true,
+    origin: ["localhost:8080"],
+}));
 app.use(express.json());
 app.use(cookieParser());
 
 // Routes
-app.use('/api/text/', check_cookies, texts.router);
-app.use('/api/link/', check_cookies, links.router);
+app.use('/api/text/', texts.router);
+app.use('/api/link/', links.router);
 app.use('/api/user/', users.router);
-app.use('/api/file/', check_cookies, files.router);
+app.use('/api/file/', files.router);
 
 app.listen(process.env.PORT || 3000);
