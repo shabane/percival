6883c869f20209b73a05c7397ba6791bef3d6449
shabane <m.mohamadshabane@gmail.com>
2024-11-20 01:27:22 +0330

    feat: call the function to delete other entries

--- grog.js
+++ grog.js
@@ -43,3 +43,12 @@ list_expire_entries(File).then(files => {
         settings.debug(data);
     }).catch(err => settings.debug(err.message));
 });
+
+
+for (let type of [Text, Link, User_Data, User_Link, User_File]) {
+    list_expire_entries(type).then(exp => {
+        Promise.all(delete_expire_entries(exp)).then(data => {
+            settings.debug(data);
+        }).catch(err => settings.debug(err.message));
+    });
+}
